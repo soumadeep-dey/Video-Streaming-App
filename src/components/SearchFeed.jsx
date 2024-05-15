bb4986901c4a861e587ca72530e1693644e60db0
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-05-15 19:02:11 +0530

    added search based feed & video player and suggestion page

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,20 +2,23 @@ import { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=`).then((data) => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
       setVideos(data.items);
     });
-  }, []);
+  }, [searchTerm]);
 
   return (
-    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}> 
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search Results for: <span style={{ color: "#9403fc" }}>videos</span>
+        Search Results for:{" "}
+        <span style={{ color: "#9403fc" }}>{searchTerm}</span> videos
       </Typography>
 
       <Videos videos={videos} />
